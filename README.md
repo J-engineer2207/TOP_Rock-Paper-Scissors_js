98e3a8c58dc2732e3627f6ef26a6f9638d96b27e
Jason <2686239@students.wits.ac.za>
2025-04-08 11:14:30 -0700

    Created a function that allows the user to input their choice

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TOP_Rock-Paper-Scissors_js
+Creating a rock-paper-scissors game. This game will be played in the console, and will be played against the computer.
--- rps_game.js
+++ rps_game.js
@@ -1,21 +1,34 @@
 // Create a random function called getComputerChoice
+//Create an if statement with a logical operator
+//In the if statement return 'rock' 
+//Create an else if statement with a logical operator
+//In the else statement return 'paper'
+//Create another elseif statement with a logical operator
+//In the elseif statement return 'scissors'
 function getComputerChoice(value) {
     let x = Math.floor(Math.random() * value) + 1;
     if (x === 1) {
         return 'rock'
+        
     } else if (x === 2) {
         return 'paper'
+        
     } else {
         return 'scissors'
+        
     }
+
 }
 getComputerChoice(3);
 console.log(getComputerChoice(3));
     
-//Create an if statement with a logical operator
-//In the if statement return 'rock' 
-//Create an elseif statement with a logical operator
-//In the elseif statement return 'paper'
-//Create another elseif statement with a logical operator
-//In the elseif statement return 'scissors' 
+ //Create a function called getHumanChoice
+ //In the function create a prompt window method with default text
+ function getHumanChoice() {
+    let result = prompt("Choose your weapon");
+    console.log("Human:",result)
+    
+ }
+ getHumanChoice();
+ console.log(getHumanChoice);
 
