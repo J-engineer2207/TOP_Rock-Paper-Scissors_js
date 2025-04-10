7b0156ccd1c2537cefed59accb1f536d103aaadf
Jason <2686239@students.wits.ac.za>
2025-04-10 00:00:35 -0700

    Created function playRound to compare arguments
    In the function there are if else-if and else
    conditions for comparing humanChoice and
    computerChoice, though the console only displays
    when I directly compare the two using ===, it does
    not display the message for other conditions.

--- rps_game.js
+++ rps_game.js
@@ -2,33 +2,65 @@
 //Create an if statement with a logical operator
 //In the if statement return 'rock' 
 //Create an else if statement with a logical operator
-//In the else statement return 'paper'
-//Create another elseif statement with a logical operator
-//In the elseif statement return 'scissors'
+//In the else if statement return 'paper'
+//Create an else statement that returns 'scissors'
 function getComputerChoice(value) {
     let x = Math.floor(Math.random() * value) + 1;
     if (x === 1) {
-        return 'rock'
+        let rock = 'rock';
+        console.log('Computer:', rock); 
         
     } else if (x === 2) {
-        return 'paper'
+        let paper = 'paper';
+        console.log('Computer:', paper);
         
     } else {
-        return 'scissors'
+        let scissors = 'scissors';
+        console.log('Computer:', scissors);
         
     }
-
 }
-getComputerChoice(3);
-console.log(getComputerChoice(3));
-    
- //Create a function called getHumanChoice
- //In the function create a prompt window method with default text
- function getHumanChoice() {
-    let result = prompt("Choose your weapon");
-    console.log("Human:",result)
-    
- }
- getHumanChoice();
- console.log(getHumanChoice);
 
+    
+//Create a function called getHumanChoice
+//In the function create a prompt window method with default text
+function getHumanChoice() {
+    let result = prompt("Choose your weapon");
+    console.log("Human:",result.toLowerCase())
+    
+}
+
+
+let humanScore  = 0;
+let computerScore = 0;
+//Create a function called playRound which has two parameters
+//Make the functions humanChoice parameter case-insensitive
+//In the function produce a string that repre
+//sents the round winner
+// Increment the computerScore or humanScore variable based on winner
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log('You Win!, Scissors beats Paper');
+        humanScore += 1;
+        console.log('Computer:'+computerScore + ' ' + 'Human:' +humanScore)
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You Win!, Paper beats Rock');
+        humanScore += 1;
+        console.log('Computer:' +computerScore + ' ' + 'Human:' +humanScore);
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You Win!, Rock beats Scissors')
+        humanScore += 1;
+        console.log('Computer:' +computerScore + ' ' + 'Human:' +humanScore);
+    } else if (humanChoice === computerChoice) {
+        console.log('Draw, Play again');
+        console.log('Computer:' +computerScore + ' ' + 'Human:' +humanScore);
+    } else {
+        console.log('You Lose!, Try again');
+        computerScore += 1;
+        console.log('Computer:' +computerScore + ' ' + 'Human:' +humanScore);
+    }
+}
+
+
+playRound(getHumanChoice(),getComputerChoice(3));
+// console.log(playRound(humanChoice,computerChoice(3)));
